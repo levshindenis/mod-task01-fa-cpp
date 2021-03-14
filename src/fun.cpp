e7dff424d9d056cc17dc8c19a1fa4753fb0c82fc
levshindenis <79909534+levshindenis@users.noreply.github.com>
2021-03-14 22:59:35 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,12 +1,73 @@
-unsigned int faStr1(const char *str)
+#include <iostream>
+unsigned int faStr1(const char* str)
 {
-    return 0;
+    int i = 0, count = 0, state = 'a';
+    bool f = false;
+    while (str[i] != '\0' && str[i] != '\n')
+    {
+        if (state == 'a' && str[i] != ' ')
+        {
+            state = 'b';
+            if (int(str[i]) > 47 && int(str[i]) < 58) f = true;
+            else f = false;
+        }
+        else if (state == 'b' && str[i] != ' ') { if (int(str[i]) > 47 && int(str[i]) < 58) f = true; }
+        else if (state == 'b' && str[i] == ' ')
+        {
+            state = 'a';
+            if (!f) count++;
+        }
+        if (state == 'b' && (str[i + 1] == '\n' || str[i + 1] == '\0')) count++;
+        i++;
+    }
+    return count;
 }
-unsigned int faStr2(const char *str)
+
+unsigned int faStr2(const char* str)
 {
-    return 0;
+    int i = 0, count = 0, state = 'a';
+    bool f = false;
+    while (str[i] != '\0' && str[i] != '\n')
+    {
+        if (state == 'a' && str[i] != ' ')
+        {
+            state = 'b';
+            if (int(str[i]) > 64 && int(str[i]) < 91) f = false; else f = true;
+        }
+        else if (state == 'b' && str[i] != ' ') { if (int(str[i]) < 97 || int(str[i]) > 122) f = true; }
+        else if (state == 'b' && str[i] == ' ')
+        {
+            state = 'a';
+            if (!f) count++;
+        }
+        if (state == 'b' && (str[i + 1] == '\n' || str[i + 1] == '\0')) count++;
+        i++;
+        i++;
+    }
+    return count;
 }
-unsigned int faStr3(const char *str)
+
+unsigned int faStr3(const char* str)
 {
-    return 0;
+    int i = 0, state = 'a';
+    double  count = 0, countc = 0;
+    while (str[i] != '\0' && str[i] != '\n')
+    {
+        if (state == 'a' && str[i] != ' ')
+        {
+            state = 'b';
+            count++;
+            countc++;
+        }
+        else if (state == 'b' && str[i] != ' ')
+        {
+            countc++;
+        }
+        else if (state == 'b' && str[i] == ' ')
+        {
+            state = 'a';
+        }
+        i++;
+    }
+    return round(countc / count);
 }
